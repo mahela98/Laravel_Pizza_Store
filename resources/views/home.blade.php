ecc2a0b1aecfe714628def3725e81053a4f2caf7
chandima <chandimaofficial@gmail.com>
2021-03-30 11:33:08 +0530

    changed the layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <p><a href="/pizza">View all pizza orders</a></p>
                 </div>
             </div>
         </div>
